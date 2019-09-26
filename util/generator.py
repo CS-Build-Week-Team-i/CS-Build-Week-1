12cbe457c786a7cd3a41d73d255f6e42b2bc0136
Krisli Dimo <krislidimo@gmail.com>
2019-09-26 17:33:36 -0500

    finished generator

--- util/generator.py
+++ util/generator.py
@@ -19,17 +19,15 @@ class Room:
         self.x = x
         self.y = y
     def __repr__(self):
-        if self.e_to is not None:
-            return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
-        return f"({self.x}, {self.y})"
+        return f"{self.id}.zfill(3), ({self.x}, {self.y})"
     def connect_rooms(self, connecting_room, direction):
         '''
         Connect two rooms in the given n/s/e/w direction
         '''
         reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
         reverse_dir = reverse_dirs[direction]
-        setattr(self, f"{direction}_to", connecting_room)
-        setattr(connecting_room, f"{reverse_dir}_to", self)
+        setattr(self, f"{direction}_to", connecting_room.id)
+        setattr(connecting_room, f"{reverse_dir}_to", self.id)
     def get_room_in_direction(self, direction):
         '''
         Connect two rooms in the given n/s/e/w direction
@@ -42,56 +40,8 @@ class World:
         self.grid = None
         self.width = 0
         self.height = 0
+        self.num_rooms = 0
         self.room_count = 0
-
-    def generate_rooms(self, size_x, size_y, num_rooms):
-        '''
-        Fill up the grid, bottom to top, in a zig-zag pattern
-        '''
-
-        # Initialize the grid
-        self.grid = [None] * size_y
-        self.width = size_x
-        self.height = size_y
-        for i in range( len(self.grid) ):
-            self.grid[i] = [None] * size_x
-
-        # Start from center (0,0)
-        mid_x = int(self.width/2) # (this will become 0 on the first step)
-        mid_y = int(self.height/2)
-
-        def propagateHelper(previous_room,x,y):
-            if y+1 < self.height and (self.grid[y+1][x] == None):
-                propagate(previous_room, x, y+1, 'n')
-            if x+1 < self.width and (self.grid[y][x+1] == None):
-                propagate(previous_room, x+1, y, 'e')
-            if x-1 >= 0 and (self.grid[y][x-1] == None):
-                propagate(previous_room, x-1, y, 's')
-            if y-1 >= 0 and (self.grid[y-1][x] == None):   
-                propagate(previous_room, x, y-1, 'w')
-
-        def propagate(previous_room, x, y, room_direction):
-            if self.room_count >= num_rooms:
-                return 'done'
-            elif self.room_count == 0:
-                room = Room(self.room_count, "A Generic Room", "This is a generic room.", x, y)
-                self.grid[mid_y][mid_x] = room
-                self.room_count += 1
-                print(f'{room.x},{room.y}')
-                return propagateHelper(room,x,y)
-            else:
-                coinFlip = random.randrange(0, 2)
-                if coinFlip == 1:
-                    room = Room(self.room_count, "A Generic Room", "This is a generic room.", x, y)
-                    self.grid[y][x] = room
-                    previous_room.connect_rooms(room, room_direction)
-                    self.room_count += 1
-                    print(f'{room.x},{room.y}')
-                    return propagateHelper(room,x,y)
-                else:
-                    return 1
-
-        propagate(None, mid_x, mid_y, None)
 
     def print_rooms(self):
         '''
@@ -149,57 +99,85 @@ class World:
 
     def new_print_rooms(self):
         reverse_grid = list(self.grid) # make a copy of the list
-        # reverse_grid.reverse()
+        reverse_grid.reverse()
 
         # top of container
-        for room in reverse_grid[0]:
-            print('*', end='')
+        # for room in reverse_grid[0]:
+        displayFormat = '-x,y-'
+        print('*'*(len(reverse_grid[0])*len(displayFormat)+4))
 
         for row in reverse_grid:
-            print('*', end='')
+            print('||', end='')
             for room in row:
-                if room:
-                    print(f'{room.x},{room.y}', end='')
-                    if room.e_to:
-                        print('-', end='')
-                    else:
-                        print(' ', end='')
-                else:
-                    print(f'    ', end='')
-            print('')
+                print('  |  ' if room and room.n_to else '     ', end='')
+            print('||')
 
+            print('||', end='')
             for room in row:
-                if room:
-                    print(f'{room.x},{room.y}', end='')
-                    if room.s_to:
-                        print(' |  ', end='')
-                    else:
-                        print('    ', end='')
-                else:
-                    print(f'    ', end='')
-            print('*')
+                print('-'if room and room.w_to else ' ', end='')
+                print(f'{room.id}'.zfill(3) if room else '...', end='')
+                print('-'if room and room.e_to else ' ', end='')
+            print('||')
+
+            print('||', end='')
+            for room in row:
+                print('  |  ' if room and room.s_to else '     ', end='')
+            print('||')
 
         # bottom of container
-        for room in reverse_grid[0]:
-            print('*')
+        print('*'*(len(reverse_grid[0])*len(displayFormat)+4))
+
+    def generate_rooms(self, size_x, size_y, num_rooms):
+        '''
+        Fill up the grid, bottom to top, in a zig-zag pattern
+        '''
+
+        # Initialize the grid
+        self.grid = [None] * size_y
+        self.width = size_x
+        self.height = size_y
+        self.num_rooms = num_rooms
+        for i in range( len(self.grid) ):
+            self.grid[i] = [None] * size_x
+
+        # Start from center (0,0)
+        mid_x = int(self.width/2) # (this will become 0 on the first step)
+        mid_y = int(self.height/2)
+
+        def propagateHelper(previous_room,x,y):
+            if y+1 < self.height and (self.grid[y+1][x] == None):
+                propagate(previous_room, x, y+1, 'n')
+            if x+1 < self.width and (self.grid[y][x+1] == None):
+                propagate(previous_room, x+1, y, 'e')
+            if x-1 >= 0 and (self.grid[y][x-1] == None):
+                propagate(previous_room, x-1, y, 'w')
+            if y-1 >= 0 and (self.grid[y-1][x] == None):   
+                propagate(previous_room, x, y-1, 's')
+
+        def propagate(previous_room, x, y, room_direction):
+            # if number of rooms needed is reached
+            if self.room_count >= num_rooms:
+                return 
+            # first execution
+            elif self.room_count == 0:
+                room0 = Room(0, "A Generic Room", "This is a generic room.", mid_x, mid_y)
+                self.room_count += 1
+                self.grid[mid_y][mid_x] = room0 # sets new room to the middle of the map
+                propagateHelper(room0,x,y)
+            else:
+                coinFlip = random.randrange(0, 2)
+                if coinFlip == 1:
+                    room = Room(self.room_count, "A Generic Room", "This is a generic room.", x, y)
+                    self.grid[y][x] = room
+                    self.room_count += 1
+                    previous_room.connect_rooms(room, room_direction)
+                    propagateHelper(room,x,y)
+
+        propagate(None, mid_x, mid_y, None)
 
 
 w = World()
-num_rooms = 100
-width = 20
-height = 20
-w.generate_rooms(width, height, num_rooms)
-w.print_rooms()
+w.generate_rooms(16, 16, 100)
+w.new_print_rooms()
 
-
-print(f"\n\nWorld\n  height: {height},\n  width: {width},\n  num_rooms: {num_rooms},\n  num_gen_rooms: {w.room_count}\n")
-
-print('n: ', w.grid[10][10].n_to, w.grid[10][10].n_to and w.grid[10][10].n_to.id)
-print('e: ', w.grid[10][10].e_to, w.grid[10][10].e_to and w.grid[10][10].e_to.id)
-print('s: ', w.grid[10][10].s_to, w.grid[10][10].s_to and w.grid[10][10].s_to.id)
-print('w: ', w.grid[10][10].w_to, w.grid[10][10].w_to and w.grid[10][10].w_to.id)
-
-print(w.grid[10][10])
-# (previous_room,x, y, room_direction)
-# start in the center
-# 
+print(f"\n\nWorld\n  height: {w.height},\n  width: {w.width},\n  num_rooms: {w.num_rooms},\n  num_gen_rooms: {w.room_count}\n")

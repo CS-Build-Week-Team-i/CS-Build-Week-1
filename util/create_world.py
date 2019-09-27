a7052c72aad631d3adfe470ef2a38f1676375b8e
Krisli Dimo <krislidimo@gmail.com>
2019-09-26 20:45:35 -0500

    added id to rooms

--- util/create_world.py
+++ util/create_world.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
-from adventure.models import Player, Room
+from adventure.models import Player, Room, Map
 from util.generator import World
-
 
 Room.objects.all().delete()
 
@@ -14,7 +13,7 @@ for row in w.grid:
 	j=0
 	for room in row:
 		if room:
-			new_room = Room(title=room.name, description=room.description, x=room.x, y=room.y)
+			new_room = Room(id=room.id, title=room.name, description=room.description, x=room.x, y=room.y)
 			new_room.save()
 			room.n_to and new_room.connectRoomByID(room.n_to, 'n')
 			room.s_to and new_room.connectRoomByID(room.s_to, 's')
@@ -22,43 +21,7 @@ for row in w.grid:
 			room.w_to and new_room.connectRoomByID(room.w_to, 'w')
 		j+=1
 	i+=1
-
-# r_outside = Room(title="Outside Cave Entrance",
-#                description="North of you, the cave mount beckons")
-
-# r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
-# passages run north and east.""")
-
-# r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
-# into the darkness. Ahead to the north, a light flickers in
-# the distance, but there is no way across the chasm.""")
-
-# r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
-# to north. The smell of gold permeates the air.""")
-
-# r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south.""")
-
-# r_outside.save()
-# r_foyer.save()
-# r_overlook.save()
-# r_narrow.save()
-# r_treasure.save()
-
-# Link rooms together
-# r_outside.connectRooms(r_foyer, "n")
-# r_foyer.connectRooms(r_outside, "s")
-
-# r_foyer.connectRooms(r_overlook, "n")
-# r_overlook.connectRooms(r_foyer, "s")
-
-# r_foyer.connectRooms(r_narrow, "e")
-# r_narrow.connectRooms(r_foyer, "w")
-
-# r_narrow.connectRooms(r_treasure, "n")
-# r_treasure.connectRooms(r_narrow, "s")
-
+	
 players=Player.objects.all()
 for p in players:
   p.currentRoom=r_outside.id
